4a8c3b917a14dbd9f4bc1d398fb6f7cd7fbe4c43
RomanJelinek <roman.jelinek91@gmail.com>
2023-08-05 13:12:06 +0200

    test fetch fix

--- src/pages/api/get-data.ts
+++ src/pages/api/get-data.ts
@@ -7,12 +7,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-
   try {
     const data = completeDataMapper(completeData, districtsRegions);
 
     res.status(200).json({ data });
   } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message });
+    res.status(500).json({ statusCode: 500, message: err.message as any });
   }
 }
