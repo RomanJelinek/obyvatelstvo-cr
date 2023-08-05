54ec92604594b41b0e3dd177197470f8d192c599
RomanJelinek <roman.jelinek91@gmail.com>
2023-08-05 13:09:26 +0200

    test fetching data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Homepage from '@/components/Homepage';
 import { GetStaticProps, GetStaticPropsContext } from 'next';
 import { completeDataMapper } from '@/context/dataContext/mappers/completeDataMapper';
@@ -7,18 +7,35 @@ import { districtsRegions } from '@/data/regions/polygons/_regionPolygons';
 import { DataContextProvider } from '@/context/dataContext/DataContext';
 import { Data, InitialData } from '@/context/dataContext/dataContextTypes';
 import { MapContextProvider } from '@/context/mapContext/MapContext';
+import axios from 'axios';
 
 interface HomeProps {
   initialData: InitialData;
 }
 
 export const Home: FC<HomeProps> = ({ initialData }) => {
+  const [dataToUse, setDataToUse] = useState<InitialData>();
+
+  useEffect(() => {
+    const getData = async () => {
+      const dataF = await axios.get('/api/get-data');
+      const { data } = dataF;
+      setDataToUse(data.data);
+    };
+    getData();
+  }, []);
   return (
-    <MapContextProvider>
-      <DataContextProvider initialData={initialData}>
-        <Homepage />
-      </DataContextProvider>
-    </MapContextProvider>
+    <>
+      {dataToUse ? (
+        <MapContextProvider>
+          <DataContextProvider initialData={dataToUse}>
+            <Homepage />
+          </DataContextProvider>
+        </MapContextProvider>
+      ) : (
+        <>loading</>
+      )}
+    </>
   );
 };
 
