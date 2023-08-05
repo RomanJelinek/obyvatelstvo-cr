82c223a20ac4257d2a8ff0d75b46512982b25acd
RomanJelinek <roman.jelinek91@gmail.com>
2023-08-05 13:11:06 +0200

    test fetch fix

--- src/pages/api/get-data.ts
+++ src/pages/api/get-data.ts
@@ -3,9 +3,14 @@ import { districtsRegions } from '@/data/regions/polygons/_regionPolygons';
 import { completeData } from '@/data/years';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
   try {
     const data = completeDataMapper(completeData, districtsRegions);
+
     res.status(200).json({ data });
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message });
